8b3cf01c9ddef231596017b8235e2b5641b88a80
DARAKU17 <theintrxvert366@gmail.com>
2026-05-09 15:08:40 +0100

    Initial commit: Kaizen wake word engine

--- scripts/utils/req_test.py
+++
@@ -1,7 +0,0 @@
-import numpy
-import librosa
-import torch
-
-print("NumPy OK")
-print("Librosa OK")
-print("PyTorch OK")
--- scripts/utils/test_gpu.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print(torch.__version__)
-print("CUDA:", torch.cuda.is_available())
-print("GPU:", torch.cuda.get_device_name(0))
